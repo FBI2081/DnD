5cbb6ba842e9f0ec48f394c209093ee1e13a25d9
Gbour Fox <07fuga@gmail.com>
2019-10-21 12:39:03 +0200

    Vert_tulajdonsag

--- cucc_tulajdonsag/vert.h
+++ cucc_tulajdonsag/vert.h
@@ -1,5 +1,21 @@
 
+typedef enum {
+    KONNYU, KOZEPES, NEHEZ
+} e_vert_tipus;
+
 typedef struct
 {
-
+    e_vert_tipus tipus;
+    //nev
+    //ertek
+    int vedelem_modosito;
+    int max_ugyesseg;
+    int modosito_vertben;
+    int varazslat_rontas_esely;
+    //tomeg
 } Vert_tulajdonsag;
+
+typedef struct
+{
+    e_targy_meret meret;
+} Vert_tulajdonsag_dinamikus;
